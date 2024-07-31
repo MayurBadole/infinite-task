def14a49b4df6b4f31997effad0745a1d66bed64
Mayur Badole <mayur.badole@ubiqc.com>
2024-07-31 14:33:13 +0530

    create weather task

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import "./App.css";
 import WeatherListHistory from "./components/History";
 import WeatherData from "./Weather";
 
+import { useState } from "react";
+
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<WeatherData />}></Route>
-          <Route path="/history" element={<WeatherListHistory />}></Route>
-          <Route path="*" element={<>page not found</>}></Route>
-        </Routes>
-      </div>
-    </Router>
-  );
+  <Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<WeatherData />}></Route>
+        <Route path="/history" element={<WeatherListHistory />}></Route>
+        <Route path="*" element={<>page not found</>}></Route>
+      </Routes>
+    </div>
+  </Router>;
 }
 
 export default App;
